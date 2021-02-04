b6bfa011abfbb2d05aac7f8c99c7d942e4e084ff
tpreston96 <tpreston96@gmail.com>
2021-02-04 17:13:39 -0600

    Tea model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Tea(models.Model):
+    name = models.CharField(max_length=100)
+    variety = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    caffeineLvl = models.IntegerField(
+        default=1, validators=[MaxValueValidator(5), MinValueValidator(1)]
+    )
+    quantity = models.IntegerField(
+        default=1, validators=[MaxValueValidator(100), MinValueValidator(1)]
+    )
+    quantPerBox = models.IntegerField(
+        default=1, validators=[MaxValueValidator(100), MinValueValidator(1)]
+    )
